9222f82ada86e79ec1e2528fe9c2265c0204af02
show2891 <show2891@gmail.com>
2023-11-21 14:02:00 +0900

    환불계좌 처리 ajax 추가중

---
+++ src/main/java/servlets/refund/command/WriteRefundAcct.java
@@ -0,0 +1,41 @@
+package servlets.refund.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import auth.AuthInfo;
+import mvc.command.CommandHandler;
+import servlets.member.service.MemberService;
+
+public class WriteRefundAcct implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("MemberChangePwd.java : 비밀번호 변경 ajax 처리");
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		HttpSession session = request.getSession(false);
+		AuthInfo auth = (AuthInfo) session.getAttribute("auth");		
+		
+		int memberNo = auth.getMemberNo();
+		String insttCode = request.getParameter("insttCode");
+		String acctOwner = request.getParameter("accountHolder");
+		String acctNum = request.getParameter("bankAccount");
+		
+//		MemberService memberService = new MemberService();
+		
+		int rowCount = 0;
+//		rowCount = memberService.writeRefundAcct(dto);
+		
+		String jsonResult = String.format("{ \"rowCount\": %d }", rowCount);
+		
+//		JSONObject jsonResult = new JSONObject();
+//		jsonResult.put("memberNo" , dto.getMemberNo());
+		
+		return jsonResult;
+	}
+
+}
