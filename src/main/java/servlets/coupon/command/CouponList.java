742f7c483d5977085430c1beac537b77b8b230a8
min9seok <wjdndd9319@naver.com>
2023-11-21 12:37:01 +0900

    작성중

---
+++ src/main/java/servlets/coupon/command/CouponList.java
@@ -0,0 +1,34 @@
+package servlets.coupon.command;
+
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import auth.AuthInfo;
+import mvc.command.CommandHandler;
+import servlets.product.domain.ProductsDTO;
+import servlets.product.service.ListService;
+
+public class CouponList implements CommandHandler{
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println(">CouponList.process ");
+		HttpSession session = request.getSession(false);
+		AuthInfo auth = (AuthInfo) session.getAttribute("auth");
+		int memberNo = auth.getMemberNo();
+				
+		ListService listService = ListService.getInstance();
+		List<ProductsDTO> reviewlist = listService.selectreview(memberNo);
+		//1.  포워딩 전 데이터 저장
+		request.setAttribute("reviewlist", reviewlist);
+
+		return "/WEB-INF/views/coupon/list.jsp";
+		
+		
+	}
+
+}
