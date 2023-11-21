a105b41cfd9b881d31971d674057c19e12d6eb0b
min9seok <wjdndd9319@naver.com>
2023-11-21 17:42:18 +0900

    수정

--- src/main/java/servlets/cart/command/CartDaily.java
+++ src/main/java/servlets/cart/command/CartDaily.java
@@ -21,39 +21,15 @@ public class CartDaily implements CommandHandler{
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+		System.out.println("> Cart start..");	
 
-		System.out.println("> Cart start..");
-
-		String jsonStr = request.getRealPath("url");
-		Pattern pattern = Pattern.compile("/cart/daily.do");
-		Matcher matcher = pattern.matcher(jsonStr);
-
-		ArrayList<String> itemCodeList = new ArrayList<>();
-		ArrayList<int []> dayQtyList = new ArrayList<>();
-
-		 // list에 데이터 담기
-        String itemCode, dayQtyStr;
-        while (matcher.find()) {
-            itemCode = matcher.group(1);
-            dayQtyStr = matcher.group(2);
-
-
-		// dayQtyStr 처리
-		String [] dayQtyArr = dayQtyStr.split(",");
-		int[] dayQty = new int[dayQtyArr.length];
-		// parseInt
-		for (int i = 0; i < dayQtyArr.length; i++) {
-			dayQty[i] = Integer.parseInt(dayQtyArr[i].trim());
-		} // for
-
-		// 추출한 데이터 리스트에 저장
-		itemCodeList.add(itemCode);
-		dayQtyList.add(dayQty);
-
-		System.out.println(itemCodeList);
-      } // while
-      
-	
+		String itemCodeLists = request.getParameter("products_no");
+		String itemCodeList = null;				
+		if (itemCodeLists != null) {
+			itemCodeList = String.join(", ",itemCodeLists);
+		}
+		System.out.println(itemCodeList);      
+      	
 		CartService cartService = CartService.getInstance();
 		ArrayList<CartDTO> list = cartService.cartList(itemCodeList);
 
